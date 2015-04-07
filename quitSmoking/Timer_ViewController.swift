00a53a1a0de0b4f38a49c5ce678f5aadaba95d61
Goran Pavlovic <goranpavlovic@Pavlovics-MacBook-Pro.local>
2015-04-07 16:38:59 -0400

    Completed timer that keeps track of the users time not smoking. Added tracking for amount of money the user has saved by not smoking for X amount of days. The user is prompted to input the average amount they pay for a pack, and how many packs they smoke per day once they click the "Start" button on the timer screen. All of this data is now persistent.

--- quitSmoking/Timer_ViewController.swift
+++ quitSmoking/Timer_ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import Foundation
 
-class Timer_ViewController: UIViewController {
+class Timer_ViewController: UIViewController, NSCoding {
     
-    var quitTime : NSDate = NSDate()
+    var quitTime : NSDate!
+    var userDidQuit = false
     @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var moneyLabel: UILabel!
+    var avgPackPrice:Double!
+    var packsPerDay:Int!
+    var moneySaved = 0.00
     
     override func viewDidLoad() {
+        let filePath = self.dataFilePath()
+        
         super.viewDidLoad()
-        updateTimer()
 
-        // Do any additional setup after loading the view.
+        if (NSFileManager.defaultManager().fileExistsAtPath(filePath)) {
+            let data = NSMutableData(contentsOfFile: filePath)!
+            let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+            // Get data
+            quitTime = unarchiver.decodeObjectForKey("quitTimeKey") as NSDate
+            userDidQuit = unarchiver.decodeBoolForKey("userQuitKey")
+            avgPackPrice = unarchiver.decodeDoubleForKey("packPriceKey")
+            packsPerDay = unarchiver.decodeIntegerForKey("packsPerDayKey")
+            unarchiver.finishDecoding()
+        }
+        
+        let app = UIApplication.sharedApplication()
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "applicationWillResignActive:",
+            name: UIApplicationWillResignActiveNotification,
+            object: app)
+        // Start updating the timer
+        update()
+        NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,23 +50,107 @@ class Timer_ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func updateTimer() {
-        var currentTime = NSDate()
-        let calendar = NSCalendar.currentCalendar()
-        let unit:NSCalendarUnit = NSCalendarUnit.CalendarUnitDay
-        let comp = calendar.components(unit, fromDate: quitTime, toDate: currentTime, options: nil)
-        
-        timerLabel.text = String(comp.day)
+    func update() {
+        if (userDidQuit) {
+            var currentTime = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            // Days
+            let unitDay:NSCalendarUnit = NSCalendarUnit.CalendarUnitDay
+            let compDay = calendar.components(unitDay, fromDate: quitTime, toDate: currentTime, options: nil)
+            // Hours
+            let unitHour:NSCalendarUnit = NSCalendarUnit.CalendarUnitHour
+            let compHour = calendar.components(unitHour, fromDate: quitTime, toDate: currentTime, options: nil)
+            // Minutes
+            let unitMin:NSCalendarUnit = NSCalendarUnit.CalendarUnitMinute
+            let compMin = calendar.components(unitMin, fromDate: quitTime, toDate: currentTime, options: nil)
+            
+            timerLabel.text = String(compDay.day)+" days "+String(compHour.hour)+" hours "+String(compMin.minute)+" minutes"
+            
+            moneySaved = Double(compDay.day) * avgPackPrice * Double(packsPerDay)
+            moneyLabel.text = String(format:"%.2f",moneySaved)+"$"
+        }
     }
     
-    func resetTimer() {
-        
+    @IBAction func resetTimer(sender: AnyObject) {
+        userDidQuit = false
+        timerLabel.text = "0 days 0 hours 0 minutes"
+        moneyLabel.text = "0.00$"
     }
     
-    func setQuitTime() {
-        var currentTime = NSDate.timeIntervalSinceDate(quitTime)
+    @IBAction func startButtonPressed(sender: AnyObject) {
+        println("YES")
+        if (!userDidQuit) {
+            println("yes")
+            userDidQuit = true
+            quitTime = NSDate()
+            
+            let message = "Congratulations on beginning the road to a healthier you!"
+            let alert = UIAlertController (
+                title: ":)",
+                message: message,
+                preferredStyle: .Alert)
+            
+            let startAction = UIAlertAction(title: "start", style: .Default) { (_) in
+                let packPriceIN = alert.textFields![0] as UITextField
+                let packNumIN = alert.textFields![1] as UITextField
+                
+                self.avgPackPrice = (packPriceIN.text as NSString).doubleValue
+                self.packsPerDay = packNumIN.text.toInt()
+            }
+            
+            alert.addTextFieldWithConfigurationHandler{ (textField) in
+                textField.placeholder = "Average Pack Price ie. 10.25"
+                textField.keyboardType = UIKeyboardType.DecimalPad
+                NSNotificationCenter.defaultCenter().addObserverForName(UITextFieldTextDidChangeNotification, object: textField, queue: NSOperationQueue.mainQueue()) { (notification) in
+                        startAction.enabled = textField.text != ""
+                    }
+            }
+            alert.addTextFieldWithConfigurationHandler{ (textField) in
+                textField.placeholder = "Average Packs per day ie. 2"
+                textField.keyboardType = UIKeyboardType.DecimalPad
+                NSNotificationCenter.defaultCenter().addObserverForName(UITextFieldTextDidChangeNotification, object: textField, queue: NSOperationQueue.mainQueue()) { (notification) in
+                    startAction.enabled = textField.text != ""
+                }
+            }
+            alert.addAction(startAction)
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func applicationWillResignActive(notification:NSNotification) {
+        let filePath = self.dataFilePath()
         
-        // Archive the currentTime as the quitTime
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        archiver.encodeObject(self.quitTime, forKey: "quitTimeKey")
+        archiver.encodeBool(self.userDidQuit, forKey: "userQuitKey")
+        archiver.encodeDouble(self.avgPackPrice, forKey: "packPriceKey")
+        archiver.encodeInteger(self.packsPerDay, forKey: "packsPerDayKey")
+        archiver.finishEncoding()
+        data.writeToFile(filePath, atomically: true)
+    }
+    
+    func dataFilePath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(
+            NSSearchPathDirectory.DocumentDirectory,
+            NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory = paths[0] as NSString
+        return documentsDirectory.stringByAppendingPathComponent("data.archive") as String
+    }
+    
+    override func encodeWithCoder(coder: NSCoder) {
+        coder.encodeObject(quitTime, forKey:"quitTimeKey")
+        coder.encodeBool(userDidQuit, forKey: "userQuitKey")
+        coder.encodeDouble(avgPackPrice, forKey: "packPriceKey")
+        coder.encodeInteger(packsPerDay, forKey: "packPerDayKey")
+    }
+    
+    required init(coder decoder:NSCoder) {
+        self.quitTime = decoder.decodeObjectForKey("quitTimeKey") as NSDate!
+        self.userDidQuit = decoder.decodeBoolForKey("userQuitKey")
+        self.avgPackPrice = decoder.decodeDoubleForKey("packPriceKey")
+        self.packsPerDay = decoder.decodeIntegerForKey("packsPerDayKey")
+        super.init(coder: decoder)
     }
     
 
